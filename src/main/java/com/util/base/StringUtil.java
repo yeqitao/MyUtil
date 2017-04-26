980fcd17504eae958f95f92eb816027bb09f0361
yeqitao <yeqitao@good321.net>
2017-04-26 09:05:42 +0800

    ...

--- src/main/java/com/util/base/StringUtil.java
+++ src/main/java/com/util/base/StringUtil.java
@@ -1,5 +1,14 @@
 package com.util.base;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class StringUtil {
 	
 	/**
@@ -11,10 +20,10 @@ public class StringUtil {
 		if(value == null || value.trim().equals("")){
 			return false;
 		}
-		if(!value.toLowerCase().equals(value)){
-			return false;
+		if(value.trim().toLowerCase().equals(value)){
+			return true;
 		}
-		return true;
+		return false;
 	}
 	
 	/**
@@ -26,10 +35,10 @@ public class StringUtil {
 		if(value == null || value.trim().equals("")){
 			return false;
 		}
-		if(!value.toUpperCase().equals(value)){
-			return false;
+		if(value.trim().toUpperCase().equals(value)){
+			return true;
 		}
-		return true;
+		return false;
 	}
 	
 	/**
@@ -80,12 +89,50 @@ public class StringUtil {
 		return sb.toString();
 	}
 	
+	/**
+	 * 判断该字符串数组是否存在该字符串
+	 * @param values		字符串数组
+	 * @param value			字符串
+	 * @return
+	 */
+	public static boolean contains(String [] values , String value){
+		if(values != null && values.length > 0 && value != null){
+			for (String str : values) {
+				if(str.equals(value)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
+	/**
+	 * 判断字符串是否为空(null,"","null")
+	 * @param value		字符串
+	 * @return 为空则返回true，否则返回false
+	 */
+	public static boolean isNullOrEmpty(String value) {
+		return value == null || value.trim().equals("") || value.trim().equals("null");
+	}
+	
+	/**
+	 * 字符串转码
+	 * @param value		字符串
+	 * @param oldCoding		旧编码
+	 * @param newCoding		新编码
+	 * @return
+	 */
+	public static String transcoding(String value , String oldCoding , String newCoding){
+		try {
+			return new String(value.getBytes(oldCoding),newCoding);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			throw new RuntimeException("字符串转码出问题了");
+		}
+	}
 	
 	public static void main(String[] args) {
-		/*String str = repeatString("abcd,",3);
-		String[]arr = str.split(",");
-		System.out.println(arr.length);*/
-		System.out.println(reverse("你是谁!"));
+		
+		System.out.println();
 	}
 }
